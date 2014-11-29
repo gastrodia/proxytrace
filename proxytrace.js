e64a1fa64293a4840ce1284b7d71a641a3ddbf14
Yang <ipv6.elata@gmail.com>
2014-11-29 14:15:04 +0800

    proxytrace

--- proxytrace.js
+++ proxytrace.js
@@ -3,54 +3,56 @@
 /**
  * Created by Yang on 2014/11/8.
  */
-/**
- * Created by yang on 14-10-28.
- */
-
-
-
 
 var http      = require('http');
 var httpProxy = require('http-proxy');
 
+var program = require('commander');
+var chalk = require('chalk');
 
-/*
+program
+    .version('0.0.1')
+    .option('-p, --port [name]', 'the port use for proxy server')
+    .option('-t, --target [target]', 'the site you want to proxy ,for exmaple http://www.baidu.com')
+    .parse(process.argv);
 
-实现方式：
- todo 是不是可以把它封装为一个命令行工具
-1.host ip临时替换
-2.端口代理
-用法 ： ptrace 8020 https://emea.universal-api.travelport.com
- */
-
-var proxy = httpProxy.createProxy();
-
-var reqAnalyze = function(req){
-    if(req.url.indexOf('uAPI')>-1){
-        console.log('url: %s', req.url);
-        console.log('method: %s',req.method);
-        var data = '';
-        req.on('data',function(chunk){
-            data += chunk;
-        });
-        req.on('end',function(){
-            console.log('data: %s',data);
-        })
-    }
+if(!program.port || !program.target) {
+    program.help();
+} else {
+    main();
 }
 
-var proxyServer = http.createServer(function(req, res) {
-    reqAnalyze(req);
-    proxy.web(req, res, {
-        target: 'https://emea.universal-api.travelport.com'
-    });
-    proxy.on('proxyRes', function (proxyRes, req, res) {
-        console.log('success_bak:');
-        console.log(res.body);
-    });
-});
 
-var port = 8020;
 
-proxyServer.listen(port);
-console.log('ProxyAttacker is listen on ' + port);
+
+function main(){
+    var proxy = httpProxy.createProxy();
+
+    var reqAnalyze = function(req){
+        if(req.url.indexOf('uAPI')>-1){
+            console.log('url: %s', req.url);
+            console.log('method: %s',req.method);
+            var data = '';
+            req.on('data',function(chunk){
+                data += chunk;
+            });
+            req.on('end',function(){
+                console.log('data: %s',data);
+            })
+        }
+    }
+
+    var proxyServer = http.createServer(function(req, res) {
+        reqAnalyze(req);
+        proxy.web(req, res, {
+            target: program.target
+        });
+        proxy.on('proxyRes', function (proxyRes, req, res) {
+            console.log('success_bak:');
+            console.log(res.body);
+        });
+    });
+    proxyServer.listen(program.port);
+    console.log(chalk.magenta.bold('proxytrace is listen on ' + program.port));
+    console.log(chalk.magenta.bold('now proxytrace is watching the site:  ' + program.target));
+}
